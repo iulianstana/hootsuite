aa4e3aae9a25831a1ca54c3fce26ef99c02dde22
Iulian <julian.stana@gmail.com>
2016-09-25 23:15:36 +0300

    [docker] add variables constants to use local or docker app

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 # config file for my app
 # make all the changes in case you want other configuration
+import os
 
 # global settings
 JSON_PATH = "app/subreddits.json"
 WAIT_TIME = 300
 
 # mongo configuration
-MONGO_SERVER = "mongo"
-MONGO_PORT = 27017
+MONGO_SERVER = os.environ['MONGO_SERVER'] if 'MONGO_SERVER' in os.environ else "0.0.0.0"
+MONGO_PORT = int(os.environ['MONGO_PORT']) if 'MONGO_PORT' in os.environ else 27017
 
-DATABASE_NAME = "hootsuite-challenge"
+DATABASE_NAME = os.environ['DATABASE_NAME'] if 'DATABASE_NAME' in os.environ else "hootsuite-challenge"
 
 # flask configuration
-WEB_SERVER = "0.0.0.0"
-WEB_PORT = 5000
+WEB_SERVER = os.environ['WEB_SERVER'] if 'WEB_SERVER' in os.environ else "0.0.0.0"
+WEB_PORT = int(os.environ['WEB_PORT']) if 'WEB_PORT' in os.environ else 5000
