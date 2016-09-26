d81f09432fa3995e88433c821199b62f9bbd98be
Iulian <julian.stana@gmail.com>
2016-09-27 00:19:23 +0300

    [config] add a function that wrap up os.environ

--- config.py
+++ config.py
@@ -2,16 +2,31 @@
 # make all the changes in case you want other configuration
 import os
 
+
+def get_environment_var(env_name, default_value):
+    """
+    Map the os.environment to return actual value or a default one.
+
+    :param env_name: environment variable that is searched
+    :param default_value: default value to be return
+    :return: env_variable
+    """
+    if env_name in os.environ:
+        return os.environ[env_name]
+    else:
+        return default_value
+
 # global settings
 JSON_PATH = "app/subreddits.json"
-WAIT_TIME = 300
+WAIT_TIME = int(get_environment_var('LONG_SCRIPT_WAIT', 300))
+
 
 # mongo configuration
-MONGO_SERVER = os.environ['MONGO_SERVER'] if 'MONGO_SERVER' in os.environ else "0.0.0.0"
-MONGO_PORT = int(os.environ['MONGO_PORT']) if 'MONGO_PORT' in os.environ else 27017
+MONGO_SERVER = get_environment_var('MONGO_SERVER', "0.0.0.0")
+MONGO_PORT = int(get_environment_var('MONGO_PORT', 27017))
 
-DATABASE_NAME = os.environ['DATABASE_NAME'] if 'DATABASE_NAME' in os.environ else "hootsuite-challenge"
+DATABASE_NAME = get_environment_var('DATABASE_NAME', "hootsuite-challenge")
 
 # flask configuration
-WEB_SERVER = os.environ['WEB_SERVER'] if 'WEB_SERVER' in os.environ else "0.0.0.0"
-WEB_PORT = int(os.environ['WEB_PORT']) if 'WEB_PORT' in os.environ else 5000
+WEB_SERVER = get_environment_var('WEB_SERVER', "0.0.0.0")
+WEB_PORT = int(get_environment_var('WEB_PORT', 5000))
